25b7ec1f50cb6935f255b188d78d5f2b8416bcaa
BSTluo <1946831552@qq.com>
2024-01-02 15:40:31 +0800

    a

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema } from 'koishi';
 
-export const name = 'iirose-self-cut'
+export const name = 'iirose-self-cut';
 
-export interface Config {}
+export interface Config { }
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
   // write your plugin here
+  ctx.command('iirose', '花园工具')
+
+  ctx.command("iirose.cut", '切除bot的一首歌').action(async v => {
+    if (v.session.platform != 'iirose') { return; }
+    await v.session.send('cut')
+  });
+
+  ctx.command("iirose.allcut", '切除bot的所有歌').action(async v => {
+    if (v.session.platform != 'iirose') { return; }
+    const bot = v.session.bot;
+    await v.session.send('cut all')
+  });
 }
